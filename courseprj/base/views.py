f1faed0fb459ff2c8a12ff05f4072e6a00d1f216
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-06-01 14:29:42 +0200

    fead: make some basic templates, and setup basic views and urls

--- courseprj/base/views.py
+++ courseprj/base/views.py
@@ -1,9 +1,19 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
+rooms = [
+    {"id": 1, "name": "Django community"},
+    {"id": 2, "name": "Dark Room"},
+    {"id": 3, "name": "Rust and how to programm"},
+]
 
 def home(request):
-    return HttpResponse("Home")
+    context = {"rooms": rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('room')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {"room": room}
+    return render(request, 'base/room.html', context)
