09997a13f090073a01ad061a198b09c312a010e9
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-06-07 19:28:49 +0200

    feat: implement room management features with forms and views

--- courseprj/base/views.py
+++ courseprj/base/views.py
@@ -1,19 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room, Topic
+from .forms import RoomForm
 # Create your views here.
-rooms = [
-    {"id": 1, "name": "Django community"},
-    {"id": 2, "name": "Dark Room"},
-    {"id": 3, "name": "Rust and how to programm"},
-]
+
 
 def home(request):
-    context = {"rooms": rooms}
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+
+    rooms = Room.objects.filter(topic__name__icontains=q)
+
+    topics = Topic.objects.all()
+
+    context = {
+        'rooms': rooms,
+        'topics': topics,
+               }
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context = {"room": room}
+    # messages = Message.objects.filter(id=pk)
+    room = Room.objects.get(id=pk)
+
+
+    context = {
+        "room": room,
+        # "messages": messages
+               }
+
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {
+        'form': form
+    }
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
