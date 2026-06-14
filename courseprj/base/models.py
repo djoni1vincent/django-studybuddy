751947efdc6b259290ce24f3cc1d304478267b2e
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-06-14 13:32:50 +0200

    feat: add participants field to Room model and update migration; enhance room template to display participants

--- courseprj/base/models.py
+++ courseprj/base/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-from django.contrib.auth.models import User
 # Create your models here.
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -9,17 +10,18 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
+
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True, max_length=50)
-    #participants =
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-updated_at', '-created_at']
+        ordering = ["-updated_at", "-created_at"]
 
     def __str__(self):
         return self.name
